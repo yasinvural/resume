5895138c97dd74f58230126233ed7d4b8ec46b22
yasinvural <yasin.vural@metu.edu.tr>
2019-04-19 22:37:35 +0300

    Updates

---
+++ precache-manifest.498a9d378090e3a675ed3e879650e802.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "1033d828152fab48fbe3",
+    "url": "/resume/static/js/main.65a17bc3.chunk.js"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/resume/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1033d828152fab48fbe3",
+    "url": "/resume/static/css/main.bf14ffca.chunk.css"
+  },
+  {
+    "revision": "9f1e23492df3b64c04e5157ca2b13728",
+    "url": "/resume/index.html"
+  }
+];
